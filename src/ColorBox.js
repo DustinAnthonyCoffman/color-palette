030926e69deba34846452fc93c41545180a119d8
Dustin Coffman <dustincoffman@Dustins-MBP.attlocal.net>
2020-09-30 17:24:35 -0500

    styles color boxes

---
+++ src/ColorBox.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './ColorBox.css';
+
+
+
+class ColorBox extends Component {
+    render() {
+        const {name, background} = this.props;
+        return (
+            <div style={{ background: this.props.background }} className="ColorBox">
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">More</span>
+            </div>
+        );
+    }
+}
+
+export default ColorBox;
--- src/Palette.js
+++ src/Palette.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
+
         return (
             <div className="Palette">
                 {/* navbar goes heree */}
                 <div className="Palette-colors">
-                    {/* bunch of color boxes */}
+                    {colorBoxes}
                 </div>
                 {/* color footer */}
             </div>
