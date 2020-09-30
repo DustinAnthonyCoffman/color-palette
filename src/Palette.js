23f41feb994293cffcfa7cdcce0d4240f489a7de
Dustin Coffman <dustincoffman@Dustins-MBP.attlocal.net>
2020-09-30 15:38:52 -0500

    creates palette component

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar goes heree */}
+                <div className="Palette-colors">
+                    {/* bunch of color boxes */}
+                </div>
+                {/* color footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
