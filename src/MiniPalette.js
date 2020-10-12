f143ac9a9fb834965eaff901c3935c1852c1b4ba
Dustin Coffman <dustincoffman@Dustins-MBP.attlocal.net>
2020-10-12 18:08:22 -0500

    adds palette routing

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -14,7 +14,10 @@ const styles = {
         }
     },
     colors: {
-        backgroundColor: "grey"
+        backgroundColor: "#dae1e4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px"
     },
     title: {
         display: "flex",
@@ -29,15 +32,28 @@ const styles = {
     emoji: {
         marginLeft: "0.rem",
         fontSize: "1.5rem"
-
+    },
+    miniColor: {
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-3.5px"
     }
 };
 
 function MiniPalette(props) {
-    const {classes, paletteName, emoji} = props;
+    const {classes, paletteName, emoji, colors} = props;
+    const miniColorBoxes = colors.map(color => (
+        <div className={classes.miniColor}
+            style={{backgroundColor: color.color}}
+            key={color.name}
+            />
+    ))
     return (
-        <div className={classes.root}>
-            <div className={classes.colors}></div>
+        <div className={classes.root} onClick={props.handleClick}>
+            <div className={classes.colors}>{miniColorBoxes}</div>
             <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
         </div>
     );
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from "@material-ui/styles";
 const styles = {
     root: {
         backgroundColor: "blue",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center"
@@ -35,6 +35,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`);
+    }
     render() {
         const { palettes, classes } = this.props;
         return (
@@ -45,8 +48,10 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette => (
-                        <MiniPalette {...palette} />
-                    ))}
+                            <MiniPalette 
+                                {...palette} 
+                                handleClick={() => this.goToPalette(palette.id)}/>
+                        ))}
                     </div>
                 </div>
             </div>
